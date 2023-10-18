7ec5bb6f3cce9bde17f2e298b180dc41ec5101f0
Lu Hui <luhui@sipeed.com>
2023-10-18 11:09:54 +0800

    lpi4a: packages: firmware: add boot firmware

--- lpi4a/packages/firmware.scm
+++ lpi4a/packages/firmware.scm
@@ -1,6 +1,7 @@
 (define-module (lpi4a packages firmware)
   #:use-module (gnu packages)
   #:use-module (guix build-system copy)
+  #:use-module (guix git-download)
   #:use-module (guix packages)
   #:use-module (guix build utils)
   #:use-module (guix profiles)
@@ -35,8 +36,28 @@
    (synopsis "firmware for rtl8723ds bluetooth")
    (description synopsis)))
 
+(define-public th1520-boot-firmware
+  (package
+   (inherit aic8800-firmware)
+   (name "th1520-boot-firmware")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+	   (url "https://github.com/revyos/th1520-boot-firmware/")
+	   (commit "ad8f2a03765b2066cbb6143ce98ddf2111b0c7bc")))
+     (file-name (string-append "th1520-boot-firmware"))
+     (sha256
+      (base32
+       "1bgs81nhyb102rpzs70w0wq5b3mm003qwp43m26hx8bchgvznw32"))))
+   (arguments
+    '(#:install-plan
+      '(("addons/boot" "boot"))))
+   (synopsis "firmware for th1520")
+   (description synopsis)))
+
 (define-public %lpi4a-firmwares
   (list
-   aic8800-firmware rtl8723ds-bt-fw))
+   aic8800-firmware rtl8723ds-bt-fw th1520-boot-firmware))
 
 (packages->manifest %lpi4a-firmwares)
